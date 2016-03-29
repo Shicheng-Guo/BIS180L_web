b66fb31b7a0879bbd7d54e312927722483a691e1
Julin Maloof <jnmaloof@ucdavis.edu>
2016-03-29 10:20:37 -0700

    fix clone_dmg.sh

--- _helper_scripts/clone_dmg.sh
+++ _helper_scripts/clone_dmg.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
+# I can't figure out how to copy from a .dmg directly, so I mount it (in the finder) and then then use the device name.
+#This has to be done manually and the script editted before running.
+
+source=/dev/disk2s1
+
+echo "found the following disks"
+
+df | grep USB
+
 disks=`df | grep USB | cut -f 1 -d " "`
 
 for disk in $disks
 do
-    echo "unmounting $disk"
-    diskutil unmountDisk $disk
+    #echo "unmounting $disk"
+    #sudo diskutil unmountDisk $disk
     echo "copying image to $disk"
-    sudo dd if=/Users/jmaloof/Desktop/BIS180L2016_home.dmg of=$disk bs=1m
+    sudo asr restore --source $source --target $disk --erase --verbose --noprompt
     echo "ejecting $disk"
+    sleep 2
     diskutil eject $disk
 done
