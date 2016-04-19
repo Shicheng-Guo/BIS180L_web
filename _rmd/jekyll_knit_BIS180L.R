47541edf7f840e5685d042941508936bd1c54110
Julin Maloof <jnmaloof@ucdavis.edu>
2016-04-19 12:40:53 -0700

    update Intro to R

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -15,4 +15,4 @@ jekyll.knit <- function(input) {
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
-jekyll.knit("_rmd/2015-05-19-genetic-networks-1.Rmd")
+jekyll.knit("_rmd/2016-04-19-R-intro.Rmd")
