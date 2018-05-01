3e9d53d36c1a4339ddb4f5b129ac8b1c40445c71
Julin Maloof <jnmaloof@ucdavis.edu>
2018-05-01 08:09:36 -0700

    fix more typos

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -15,4 +15,4 @@ jekyll.knit <- function(input) {
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
-jekyll.knit("_rmd/2017-06-01-0-genetic-networks-2.Rmd")
+jekyll.knit("_rmd/2018-05-01-Rice-GWAS.Rmd")
