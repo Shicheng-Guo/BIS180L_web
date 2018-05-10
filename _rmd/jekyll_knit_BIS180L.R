2354c7391f74aba7f1cfb57b89d5e701cea0865b
Julin Maloof <jnmaloof@ucdavis.edu>
2018-05-10 10:41:51 -0700

    Update Shiny Lab

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -15,4 +15,4 @@ jekyll.knit <- function(input) {
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
-jekyll.knit("_rmd/2018-05-01-0-Rice-SNPs.Rmd")
+jekyll.knit("_rmd/2018-05-10-Shiny.Rmd")
