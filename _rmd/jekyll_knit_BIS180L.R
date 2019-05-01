7f163fafc87231c8d91aa7c966f25f4a783da311
Julin Maloof <jnmaloof@ucdavis.edu>
2019-05-01 14:34:30 -0700

    update GWAS lab

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -15,4 +15,4 @@ jekyll.knit <- function(input) {
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
-jekyll.knit("_rmd/2019-04-30-0-Rice-SNPs.Rmd")
+jekyll.knit("_rmd/2019-05-02-Rice-GWAS.Rmd")
